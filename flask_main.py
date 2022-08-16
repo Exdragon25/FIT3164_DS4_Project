63f3dd65dc472bea5dee8494223d0c4cdbbc50cd
tche0043 <tche0043@student.monash.edu>
2022-08-16 13:28:04 +1000

    rewrite the searching algorithm

--- flask_main.py
+++ flask_main.py
@@ -64,407 +64,51 @@ def render_result(input_dict):
     :return: a render_template function with a new web pages
     处理多种可能输入
     """
-    result = None
     client = MongoClient()
     db = client.fit3164
     dish_coll = db.Dish_collection
 
     print(input_dict)
 
-    userinput = input_dict["Search"]  # String
+    user_input = input_dict["Search"]  # String
     cuisine_input = input_dict["Cuisine"]  # List = 1
     taste_input = input_dict["Taste"]  # list <= 2
     course_input = input_dict["Course"]  # list <= 3
 
-    taste_coll = []
-    for tas in taste_input:
-        temp = "taste." + str(tas)
-        taste_coll.append(temp)
-    print(taste_coll)
+    if user_input == "":
+        user_input = "."
 
-    print("有什么情况")
-    print(len(taste_coll), len(cuisine_input), len(course_input))
-    situation_marker = str(len(taste_coll)) + str(len(cuisine_input)) + str(len(course_input))
-
-    result = dish_coll.find({'name': {"$regex": userinput, "$options": "$i"}})
-
-    # Here start handle situation.
-    # 0 0 0
-    if situation_marker == "000":
-        print("000")
-        if len(userinput) == 0:
-            result = "Please do some selection"
-        else:
-            result = dish_coll.find({'name': {"$regex": userinput, "$options": "$i"}})
-    # 0 0 1
-    elif situation_marker == "001":
-        print('001')
-        if len(userinput) == 0:
-            result = dish_coll.find(
-                {"course": course_input}
-            )
-        else:
-            result = dish_coll.find(
-                {'name': {"$regex": userinput, "$options": "$i"},
-                 "course": course_input}
-            )
-    # 0 0 2
-    elif situation_marker == "002":
-        print('002')
-        if len(userinput) == 0:
-            result = dish_coll.find(
-                {"$and": [{"course": course_input[0]},
-                          {"course": course_input[1]}]})
-        else:
-            result = dish_coll.find(
-                {'name': {"$regex": userinput, "$options": "$i"},
-                 "$and": [{"course": course_input[0]},
-                          {"course": course_input[1]}]}
-            )
-    # 0 0 3
-    elif situation_marker == "003":
-        print('003')
-        if len(userinput) == 0:
-            result = dish_coll.find(
-                {"$and": [{"course": course_input[0]},
-                          {"course": course_input[1]},
-                          {"course": course_input[2]}]}
-            )
-        else:
-            result = dish_coll.find(
-                {'name': {"$regex": userinput, "$options": "$i"},
-                 "$and": [{"course": course_input[0]},
-                          {"course": course_input[1]},
-                          {"course": course_input[2]}]}
-            )
-    # -------------------------------------
-    # 0 1 0
-    elif situation_marker == "010":
-        print('010')
-        if len(userinput) == 0:
-            result = dish_coll.find({"cuisine": cuisine_input[0]})
-        else:
-            result = dish_coll.find({'name': {"$regex": userinput, "$options": "$i"}, "cuisine": cuisine_input[0]})
-    # 0 1 1
-    elif situation_marker == "011":
-        print('011')
-        if len(userinput) == 0:
-            result = dish_coll.find(
-                {"cuisine": cuisine_input[0], "course": course_input}
-            )
-        else:
-            result = dish_coll.find(
-                {'name': {"$regex": userinput, "$options": "$i"},
-                 "cuisine": cuisine_input[0],
-                 "course": course_input}
-            )
-    # 0 1 2
-    elif situation_marker == "012":
-        print('012')
-        if len(userinput) == 0:
-            result = dish_coll.find(
-                {"cuisine": cuisine_input[0],
-                 "$and": [{"course": course_input[0]},
-                          {"course": course_input[1]}]})
-        else:
-            result = dish_coll.find(
-                {'name': {"$regex": userinput, "$options": "$i"},
-                 "cuisine": cuisine_input[0],
-                 "$and": [{"course": course_input[0]},
-                          {"course": course_input[1]}]}
-            )
-    # 0 1 3
-    elif situation_marker == "013":
-        print('013')
-        if len(userinput) == 0:
-            result = dish_coll.find(
-                {"cuisine": cuisine_input[0],
-                 "$and": [{"course": course_input[0]},
-                          {"course": course_input[1]},
-                          {"course": course_input[2]}]}
-            )
-        else:
-            result = dish_coll.find(
-                {'name': {"$regex": userinput, "$options": "$i"},
-                 "cuisine": cuisine_input[0],
-                 "$and": [{"course": course_input[0]},
-                          {"course": course_input[1]},
-                          {"course": course_input[2]}]}
-            )
-    # 000 - 013 above
-    # 100 - 113 below
-    # 1 0 0
-    elif situation_marker == "100":
-        print('100')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8},
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8},
-            })
-    # 1 0 1
-    elif situation_marker == "101":
-        print('101')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8},
-                "course": course_input[0]  # conjunction
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8},
-                "course": course_input[0]
-            })
-    # 1 0 2
-    elif situation_marker == "102":
-        print('102')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8},
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]}]  # conjunction
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8},
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]}]
-            })
-    # 1 0 3
-    elif situation_marker == "103":
-        print('103')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8},
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]},
-                         {"course": course_input[2]}]  # conjunction
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8},
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]},
-                         {"course": course_input[2]}]
-            })
-    # -------------------------------------
-    # 1 1 0
-    elif situation_marker == "110":
-        print('110')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8},
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8},
-            })
-    # 1 1 1
-    elif situation_marker == "111":
-        print('111')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8},
-                "cuisine": cuisine_input[0],
-                "course": course_input[0]  # conjunction
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8},
-                "cuisine": cuisine_input[0],
-                "course": course_input[0]
-            })
-    # 1 1 2
-    elif situation_marker == "112":
-        print('112')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8},
-                "cuisine": cuisine_input[0],
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]}]  # conjunction
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8},
-                "cuisine": cuisine_input[0],
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]}]
-            })
-    # 1 1 3
-    elif situation_marker == "113":
-        print('113')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8},
-                "cuisine": cuisine_input[0],
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]},
-                         {"course": course_input[2]}]  # conjunction
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8},
-                "cuisine": cuisine_input[0],
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]},
-                         {"course": course_input[2]}]
-            })
-
-    # 200 - 213 below:
-
-    elif situation_marker == "200":
-        print('200')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-            })
-    # 2 0 1
-    elif situation_marker == "201":
-        print('201')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "course": course_input[0]
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "course": course_input[0]
-            })
-    # 2 0 2
-    elif situation_marker == "202":
-        print('202')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]}]
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]}]
-            })
-    # 2 0 3
-    elif situation_marker == "203":
-        print('203')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]},
-                         {"course": course_input[2]}]
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]},
-                         {"course": course_input[2]}]
-            })
-    # -------------------------------------
-    # 2 1 0
-    elif situation_marker == "210":
-        print('210')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-            })
-    # 2 1 1
-    elif situation_marker == "211":
-        print('211')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "course": course_input[0]
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "course": course_input[0]
-            })
-    # 2 1 2
-    elif situation_marker == "212":
-        print('212')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]}]
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]}]
-            })
-    # 2 1 3
-    elif situation_marker == "213":
-        print('213')
-        if len(userinput) == 0:
-            result = dish_coll.find({
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]},
-                         {"course": course_input[2]}]
-            })
-        else:
-            result = dish_coll.find({
-                'name': {"$regex": userinput, "$options": "$i"},
-                "cuisine": cuisine_input[0],
-                taste_coll[0]: {"$gte": 0.8}, taste_coll[1]: {"$gte": 0.8},  # conjunction
-                "$and": [{"course": course_input[0]},
-                         {"course": course_input[1]},
-                         {"course": course_input[2]}]
-            })
-
-    print(result)
-
+    if len(cuisine_input) == 0 and len(course_input) == 0:
+        result = dish_coll.find({'name': {"$regex": user_input, "$options": "$i"}})
+    elif len(cuisine_input) == 0 and len(course_input) != 0:
+        result = dish_coll.find({'name': {"$regex": user_input, "$options": "$i"},
+                                 "course": {'$in': course_input}})
+    elif len(cuisine_input) != 0 and len(course_input) == 0:
+        result = dish_coll.find({'name': {"$regex": user_input, "$options": "$i"},
+                                 "cuisine": {"$in": cuisine_input}})
+    else:
+        result = dish_coll.find({'name': {"$regex": user_input, "$options": "$i"}, "cuisine": {"$in": cuisine_input},
+                                 "course": {'$in': course_input}})
     output_coll = []
+
     if result is not None:
         if result == "Please do some selection":
-            return result
+            return output_coll
         else:
             for doc in result:
-                output_coll.append(doc['name'])
+                add = True
+                for i in taste_input:
+                    if doc['taste'] is not None:
+                        if doc['taste'].get(i) is None:
+                            add = False
+                            break
+                        if doc['taste'].get(i) < 0.8:
+                            add = False
+                            break
+                if add:
+                    output_coll.append(doc['name'])
     pprint(output_coll)
-    return output_coll  # {'name': 'Three-Pea Salad'}
+    return output_coll
 
 
 def update_database():
--- test.py
+++ test.py
@@ -1,11 +1,45 @@
 from pymongo import MongoClient
 from pprint import pprint
 
-
 client = MongoClient()
 db = client.fit3164
 dish_coll = db.Dish_collection
 
-result = dish_coll.find({"cuisine":{"$in":['American','Chinese']}}, {"course":{"$in":['Salad']}})
-for doc in result:
-    print(doc)
+user_Input = "chicken"
+cuisine_input = ["Chinese"]
+course_input = []
+taste_input = ["sweet", "salty"]
+
+if user_Input == "":
+    user_Input = "."
+
+if len(cuisine_input) == 0 and len(course_input) == 0:
+    result = dish_coll.find({'name': {"$regex": user_Input, "$options": "$i"}})
+elif len(cuisine_input) == 0 and len(course_input) != 0:
+    result = dish_coll.find({'name': {"$regex": user_Input, "$options": "$i"},
+                             "course": {'$in': course_input}})
+elif len(cuisine_input) != 0 and len(course_input) == 0:
+    result = dish_coll.find({'name': {"$regex": user_Input, "$options": "$i"},
+                             "cuisine": {"$in": cuisine_input}})
+else:
+    result = dish_coll.find({'name': {"$regex": user_Input, "$options": "$i"}, "cuisine": {"$in": cuisine_input},
+                             "course": {'$in': course_input}})
+output_coll = []
+
+if result is not None:
+    if result == "Please do some selection":
+        pass
+    else:
+        for doc in result:
+            add = True
+            for i in taste_input:
+                if doc['taste'] is not None:
+                    if doc['taste'].get(i) is None:
+                        add = False
+                        break
+                    if doc['taste'].get(i) < 0.8:
+                        add = False
+                        break
+            if add:
+                output_coll.append(doc['name'])
+pprint(output_coll)
